cc0e042f52a50cf199fc17fd9b94de6d62973d49
RDW <duckwhale@mail.ru>
2021-11-01 15:49:03 +0100

    Add a somewhat dubious test for detecting memory leaks
    I'm not convinced it's actually going to help much, but it can always be removed later if it turns out to be a problem.

--- Tests/API/test-create.lua
+++ Tests/API/test-create.lua
@@ -1,9 +1,25 @@
 local llhttp = _G.llhttp
+
+collectgarbage()
+local preCreateMemoryUsage = collectgarbage("count")
+-- printf("Memory used before creating a parser instance: %s kB", preCreateMemoryUsage)
+
 local parser = llhttp.create()
 
-dump(parser) -- todo remove
+local memoryUsage = collectgarbage("count")
+-- printf("Memory used after creating a parser instance: %s kB", memoryUsage)
 
 assert(type(parser.state) == "cdata", "Should set up the local state for the parser instance")
 assert(type(parser.settings) == "cdata", "Should set up a settings table for the parser instance")
+
+parser = nil
+collectgarbage()
+
+local postDestroyMemoryUsage = collectgarbage("count")
+-- printf("Memory used after destroying a parser instance: %s kB", postDestroyMemoryUsage)
+
+local memoryUsageDifference = postDestroyMemoryUsage - preCreateMemoryUsage
+-- This is somewhat sketchy, but as long as the test case itself isn't messing things up it should still work?
+assert(memoryUsageDifference <= 0, " Should not leak memory when parser instances are destroyed (post-GC)")
 
 printf("%s\t%s", transform.green("OK"), transform.yellow("parser.create"))
