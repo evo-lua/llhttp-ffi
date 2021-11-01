8e876c6c4ea2521ef624f44d0ff8985461100374
RDW <duckwhale@mail.ru>
2021-11-01 15:09:37 +0100

    Update the ffi build script to use compiler optimizations

--- make.cmd
+++ make.cmd
@@ -1,1 +1,7 @@
-cl /LD LinkedList.c CallbackHandlers.c -o llhttp_ffi.dll /link /DEF:llhttp_ffi.def
+@echo OFF
+cl /nologo /LD /O2 /Ot LinkedList.c CallbackHandlers.c -o llhttp_ffi.dll /link /DEF:llhttp_ffi.def
+
+REM Some optional cleanup to save disk space
+if exist *.obj del *.obj
+if exist *.exp del *.exp
+if exist *.lib del *.lib
