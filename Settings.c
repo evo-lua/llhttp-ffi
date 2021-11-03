263cbd6b26fef4ba2d7e53bf1471208d57dc1580
RDW <duckwhale@mail.ru>
2021-11-04 00:15:10 +0100

    Add more information to debug outputs

--- Settings.c
+++ Settings.c
@@ -1,3 +1,8 @@
 #define ENABLE_DEBUG_MODE 1
-#define DEBUG(fmt, ...) \
-     do { if (ENABLE_DEBUG_MODE) printf(fmt, __VA_ARGS__); } while (0)
+#define DEBUG(formatString, ...) \
+	do { \
+		if (ENABLE_DEBUG_MODE) { \
+			printf(formatString, __VA_ARGS__); \
+			printf("\t@ %s:%i\n", __FILE__, __LINE__); \
+		} \
+	} while (0)
