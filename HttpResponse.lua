53489aac0c2153815c15b4a8e83c6508df99d1ea
RDW <duckwhale@mail.ru>
2022-06-09 07:18:31 +0200

    Create HttpResponse.lua

---
+++ HttpResponse.lua
@@ -0,0 +1,9 @@
+local HttpResponse = {}
+
+function HttpResponse:Construct()
+	-- shared: headers, body
+	-- statusLine: versionString, statusCode, statusText
+	-- __tostring
+end
+
+return HttpResponse
