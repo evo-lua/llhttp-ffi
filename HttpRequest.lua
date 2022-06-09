d5e7d4f7ab3726a1baa42617b83ce934111f43a4
RDW <duckwhale@mail.ru>
2022-06-09 07:18:35 +0200

    Create HttpRequest.lua

---
+++ HttpRequest.lua
@@ -0,0 +1,9 @@
+local HttpRequest = {}
+
+function HttpRequest:Construct()
+	-- shared: headers, body
+	-- statusLine: method, requestedURL/queryString, versionString
+	-- __tostring
+end
+
+return HttpRequest
